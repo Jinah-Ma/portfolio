a33a2da696a74e5fcc84d5d77ec5a279232fd26b
Jinah-Ma <1618wlsdk@gmail.com>
2023-10-08 17:26:38 +0900

    애니메이션 구현중

---
+++ jqurey/index.js
@@ -0,0 +1,25 @@
+let btn = $('header span.ham');
+let open = $('header .gnb');
+let solid = $('header .solid');
+let before = $('header .solid:before');
+let after = $('header .solid:after');
+
+let isOpen = false;
+
+btn.on('click', function () {
+    if (!isOpen) {
+        open.css('display', 'block'),
+            open.css('width', '20rem'),
+            open.css('z-index', '999'),
+            btn.css('left', '22rem'),
+            solid.css('left', '23.5rem');
+    } else {
+        open.css('display', ''),
+            open.css('width', ''),
+            open.css('z-index', ''),
+            btn.css('left', ''),
+            solid.css('left', '');
+    }
+    isOpen = !isOpen;
+})
+
