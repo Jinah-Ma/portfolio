91b7093e423f939d5c784dd533a1bf9a71d315da
Jinah-Ma <1618wlsdk@gmail.com>
2023-10-08 20:35:52 +0900

    오류 수정

--- jqurey/index.js
+++ jqurey/index.js
@@ -1,8 +1,6 @@
 let btn = $('header span.ham');
 let open = $('header .gnb');
 let solid = $('header .solid');
-let before = $('header .solid:before');
-let after = $('header .solid:after');
 
 let isOpen = false;
 
@@ -12,14 +10,15 @@ btn.on('click', function () {
             open.css('width', '20rem'),
             open.css('z-index', '999'),
             btn.css('left', '22rem'),
-            solid.css('left', '23.5rem');
+            solid.css('left', '23.5rem'),
+            btn.addClass('active');
     } else {
         open.css('display', ''),
             open.css('width', ''),
             open.css('z-index', ''),
             btn.css('left', ''),
-            solid.css('left', '');
+            solid.css('left', ''),
+            btn.removeClass('active');
     }
     isOpen = !isOpen;
-})
-
+});
